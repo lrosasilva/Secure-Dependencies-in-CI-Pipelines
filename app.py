8085dad68f1639618d59b9787dc128bb13061ecd
lrosasilva <lrosasilva@cpp.edu>
2025-11-15 21:32:21 -0800

    Initial commit with vulnerable dependencies

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from utils import add_numbers, reverse_string
+from def import add_numbers, reverse_string
 
 def main():
-    print("The sum of 2 and 3 is", add_numbers(2, 3))
-    print("Reverse 'Hello' =", reverse_string("Hello"))
+    print("The sum of 2 and 3 is ", add_numbers(2, 3))
+    print("Reverse of 'hello' =", reverse_string("hello"))
 
 if __name__ == "__main__":
     main()
--- test_def.py
+++ test_def.py
@@ -1,10 +1,8 @@
-from utils import add_numbers, reverse_string
+from def import add_numbers, reverse_string
 
 def test_add_numbers():
     assert add_numbers(2, 3) == 5
-    assert add_numbers(-1, 1) == 0
 
 def test_reverse_string():
-    assert reverse_string("abc") == "cba"
-    assert reverse_string("Hello") == "olleH"
-    assert reverse_string("yes") == "sey"
+    assert reverse_string("hello") == "olleh"
+
