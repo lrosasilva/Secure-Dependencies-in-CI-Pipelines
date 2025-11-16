880738bc23099dfa393b2d1debb9897b0ef0071a
lrosasilva <lrosasilva@cpp.edu>
2025-11-15 21:41:15 -0800

    Renamed files to acceptable forms for importing

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from def import add_numbers, reverse_string
+from utils import add_numbers, reverse_string
 
 def main():
     print("The sum of 2 and 3 is ", add_numbers(2, 3))
--- test_utils.py
+++ test_utils.py
@@ -1,4 +1,4 @@
-from def import add_numbers, reverse_string
+from utils import add_numbers, reverse_string
 
 def test_add_numbers():
     assert add_numbers(2, 3) == 5
